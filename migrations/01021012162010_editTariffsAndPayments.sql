6b55c2b21b0f385a21406329a2a73bf74e2e4792
Золотарев Александр <Lichcomp@mail.ru>
2010-12-17 16:43:01 +0300

    Исправлена миграция

--- migrations/01021012162010_editTariffsAndPayments.sql
+++ migrations/01021012162010_editTariffsAndPayments.sql
@@ -11,20 +11,3 @@ CREATE TABLE `internet`.`Agents` (
 ENGINE = InnoDB;
 
 INSERT INTO `internet`.`AccessCategories` (`Name`, `ReduceName`) VALUES ('Просмотр паспортных данных', 'VP');
-
-ALTER TABLE `internet`.`AccessCategories` ADD COLUMN `Code` INT(10) UNSIGNED NOT NULL AFTER `ReduceName`;
-
-UPDATE `internet`.`AccessCategories` SET `Code`='1' WHERE `Id`='1';
-
-UPDATE `internet`.`AccessCategories` SET `Code`='2' WHERE `Id`='3';
-
-UPDATE `internet`.`AccessCategories` SET `Code`='3' WHERE `Id`='5';
-
-UPDATE `internet`.`AccessCategories` SET `Code`='4' WHERE `Id`='7';
-
-UPDATE `internet`.`AccessCategories` SET `Code`='5' WHERE `Id`='9';
-
-UPDATE `internet`.`AccessCategories` SET `Code`='6' WHERE `Id`='11';
-
-UPDATE `internet`.`AccessCategories` SET `Code`='7' WHERE `Id`='13';
-
