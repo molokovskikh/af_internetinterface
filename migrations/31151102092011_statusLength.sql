4861c0589081097247ad31944f887855a0abf99b
Золотарев Александр <a.zolotarev@analit.net>
2011-02-09 11:18:01 +0300

    добавил миграцию

---
+++ migrations/31151102092011_statusLength.sql
@@ -0,0 +1,5 @@
+﻿ALTER TABLE `internet`.`Status` MODIFY COLUMN `Name` VARCHAR(100) CHARACTER SET cp1251 COLLATE cp1251_general_ci NOT NULL;
+
+update internet.`Status` S set
+s.Name = "Зарегистрирован, но нет информации о подключении"
+where id = 1;
