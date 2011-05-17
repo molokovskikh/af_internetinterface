91a1c1c4d608d5bee8d489dc757fe3e273cd3798
Квасов Роман <r.kvasov@analit.net>
2011-05-17 15:19:31 +0400

    добавил проект для генерации номеров платежных карт

---
+++ migrations/137_00_payment_cards.sql
@@ -0,0 +1,8 @@
+    create table Internet.PaymentCards (
+        Id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+       Serial VARCHAR(255),
+       Pin VARCHAR(255),
+       Sum NUMERIC(19,5),
+       GenerateAt DATETIME,
+       primary key (Id)
+    );
---
+++ src/CardGenerator/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Castle.ActiveRecord;
+using Castle.ActiveRecord.Framework;
+using Castle.ActiveRecord.Framework.Config;
+using Environment = NHibernate.Cfg.Environment;
+
+namespace CardGenerator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Init();
+
+			var sumToCount = new Dictionary<decimal, int> {
+				{50, 2000},
+				{100, 2000},
+				{250, 2000},
+				{300, 2000},
+				{500, 3000},
+				{700, 1000},
+			};
+
+			var cards = Generate(sumToCount);
+			Save(cards);
+		}
+
+		private static void Init()
+		{
+			var configuration = new InPlaceConfigurationSource();
+			configuration.PluralizeTableNames = true;
+			configuration.Add(typeof (ActiveRecordBase),
+				new Dictionary<string, string>
+				{
+					{Environment.Dialect, "NHibernate.Dialect.MySQLDialect"},
+					{Environment.ConnectionDriver, "NHibernate.Driver.MySqlDataDriver"},
+					{Environment.ConnectionProvider, "NHibernate.Connection.DriverConnectionProvider"},
+					{Environment.ConnectionStringName, "DB"},
+					{Environment.ProxyFactoryFactoryClass, "NHibernate.ByteCode.Castle.ProxyFactoryFactory, NHibernate.ByteCode.Castle" },
+					{Environment.Hbm2ddlKeyWords, "none"},
+				});
+			ActiveRecordStarter.Initialize(
+				new [] {typeof (PaymentCard).Assembly},
+				configuration);
+		}
+
+		private static List<PaymentCard> Generate(Dictionary<decimal, int> sumToCount)
+		{
+			var cards = new List<PaymentCard>();
+			foreach (var pair in sumToCount)
+			{
+				for(var i = 0; i < pair.Value; i++)
+				{
+					PaymentCard existsCard;
+					PaymentCard card;
+					do
+					{
+						card = new PaymentCard(pair.Key);
+						existsCard = cards.FirstOrDefault(c => String.Equals(c.Serial, card.Serial, StringComparison.InvariantCultureIgnoreCase));
+					} while (existsCard != null);
+					cards.Add(card);
+					ActiveRecordMediator<PaymentCard>.Save(card);
+				}
+			}
+			return cards;
+		}
+
+		private static void Save(List<PaymentCard> cards)
+		{
+			foreach (var group in cards.GroupBy(c => c.Sum))
+			{
+				var name = String.Format("{0}.txt", group.Key.ToString().PadLeft(6, '0'));
+				using (var stream = new StreamWriter(File.OpenWrite(name), Encoding.GetEncoding(1251)))
+				{
+					foreach (var card in group)
+					{
+						stream.WriteLine("{0}\t{1}", card.Serial, card.Pin);
+					}
+				}
+			}
+		}
+	}
+
+	[ActiveRecord(Schema = "Internet")]
+	public class PaymentCard
+	{
+		private static Random random = new Random();
+		private const string chars = "23456789qwertyupasdfghjkzxcvbnm";
+
+		public PaymentCard()
+		{}
+
+		public PaymentCard(decimal sum)
+		{
+			Sum = sum;
+			GenerateAt = DateTime.Now;
+			Serial = RandomDigits(12);
+			Pin = RandomDigits(6);
+		}
+
+		[PrimaryKey]
+		public uint Id { get; set; }
+
+		[Property]
+		public string Serial { get; set; }
+
+		[Property]
+		public string Pin { get; set; }
+
+		[Property]
+		public decimal Sum { get; set; }
+
+		[Property]
+		public DateTime GenerateAt { get; set; }
+
+		public string RandomDigits(int lenght)
+		{
+			if (chars.Length == 0)
+				throw new Exception("Не заданы символы для генерации");
+
+			string result = "";
+			while (result.Length <= lenght)
+			{
+				result += chars[random.Next(0, chars.Length - 1)];
+			}
+			return result;
+		}
+	}
+}
