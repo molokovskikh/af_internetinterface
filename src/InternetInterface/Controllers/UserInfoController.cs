69835cb62f9e7afe191cf6b718a4e02653a76140
Золотарев Александр <a.zolotarev@analit.net>
2011-08-16 10:06:24 +0400

    Сделал вывод номера порта и счета в форме наряда

--- src/InternetInterface/Controllers/UserInfoController.cs
+++ src/InternetInterface/Controllers/UserInfoController.cs
@@ -369,6 +369,7 @@ namespace InternetInterface.Controllers
 				PropertyBag["Client"] = client;
 				PropertyBag["Password"] = Password;
 				PropertyBag["AccountNumber"] = _client.Id.ToString("00000");
+			    PropertyBag["ConnectInfo"] = _client.GetConnectInfo();
 				//PropertyBag["ConnectSumm"] = connectSumm;
 				RenderView("ClientRegisteredInfo");
 			}
