f17a9669b62ecf2baa0798178c2652789bf74f28
Золотарев Александр <a.zolotarev@analit.net>
2011-08-17 18:37:17 +0400

    поправил логирование ошибок при запуске биллинга

--- src/BillingService/Program.cs
+++ src/BillingService/Program.cs
@@ -3,22 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
+using log4net;
 
 namespace BillingService
 {
 	static class Program
 	{
+        private static ILog _log = LogManager.GetLogger(typeof(Program));
 		/// <summary>
 		/// The main entry point for the application.
 		/// </summary>
 		static void Main()
 		{
-			ServiceBase[] ServicesToRun;
-			ServicesToRun = new ServiceBase[] 
-			{ 
-				new BllingService() 
-			};
-			ServiceBase.Run(ServicesToRun);
+            try
+            {
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[] {
+                                                      new BllingService()
+                                                  };
+                ServiceBase.Run(ServicesToRun);
+            }
+            catch (Exception e)
+            {
+                _log.Error("Ошибка при инициализации приложения", e);
+            }
 		}
 	}
 }
