40fb81e06ae777018c33810d55b3895d8e23bd97
Золотарев Александр <a.zolotarev@analit.net>
2011-11-02 10:20:14 +0400

    Удалил коменты

--- src/InforoomInternet/Global.asax.cs
+++ src/InforoomInternet/Global.asax.cs
@@ -22,7 +22,7 @@ using log4net.Config;
 
 namespace InforoomInternet
 {
-	public class Global : WebApplication, IMonoRailConfigurationEvents, IMonoRailContainerEvents
+	public class Global : WebApplication, IMonoRailConfigurationEvents
 	{
 		private static readonly ILog _log = LogManager.GetLogger(typeof(Global));
 
@@ -68,57 +68,6 @@ namespace InforoomInternet
 
 		void Application_AuthenticateRequest(object sender, EventArgs e) { }
 
-		/*void Application_Error(object sender, EventArgs e) {
-			var exception = Server.GetLastError();
-
-			if (exception is ControllerNotFoundException
-				&& !Request.UrlReferrer.AbsolutePath.Contains("ivrn.net"))
-			{
-				return;
-			}
-
-			var builder = new StringBuilder();
-			builder.AppendLine("----UrlReferer-------");
-			builder.AppendLine(Request.UrlReferrer != null ? Request.UrlReferrer.ToString() : String.Empty);
-			builder.AppendLine("----Url-------");
-			builder.AppendLine(Request.Url.ToString());
-			builder.AppendLine("--------------");
-			builder.AppendLine("----Params----");
-			foreach (string name in Request.QueryString)
-				builder.AppendLine(String.Format("{0}: {1}", name, Request.QueryString[name]));
-			builder.AppendLine("--------------");
-
-			builder.AppendLine("----Error-----");
-			do {
-				builder.AppendLine("Message:");
-				builder.AppendLine(exception.Message);
-				builder.AppendLine("Stack Trace:");
-				builder.AppendLine(exception.StackTrace);
-				builder.AppendLine("--------------");
-				exception = exception.InnerException;
-			} while (exception != null);
-			builder.AppendLine("--------------");
-
-			builder.AppendLine("----Session---");
-			try {
-				foreach (string key in Session.Keys) {
-					if (Session[key] == null)
-						builder.AppendLine(String.Format("{0} - null", key));
-					else
-						builder.AppendLine(String.Format("{0} - {1}", key, Session[key]));
-				}
-			}
-			catch (Exception ex) { }
-			builder.AppendLine("--------------");
-
-			_log.Error(builder.ToString());
-/*
-#if !DEBUG
-			Response.Redirect("~/Rescue/Error.aspx");
-#endif
-*/
-		//}
-
 		void Session_End(object sender, EventArgs e) { }
 
 		void Application_End(object sender, EventArgs e) { }
@@ -133,13 +82,5 @@ namespace InforoomInternet
 			configuration.ViewEngineConfig.VirtualPathRoot = configuration.ViewEngineConfig.ViewPathRoot;
 			configuration.ViewEngineConfig.ViewPathRoot = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, configuration.ViewEngineConfig.ViewPathRoot);
 		}
-
-		/*public void Created(IMonoRailContainer container)
-		{ }*/
-
-		/*public void Initialized(IMonoRailContainer container)
-		{
-			((DefaultViewComponentFactory)container.GetService<IViewComponentFactory>()).Inspect(Assembly.Load("InforoomInternet"));
-		}*/
 	}
 }
