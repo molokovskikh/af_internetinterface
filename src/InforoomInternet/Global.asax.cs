4fb446ae4119bdad6a002a01c5525e1d00300a31
Золотарев Александр <a.zolotarev@analit.net>
2012-05-23 17:07:03 +0400

    Выполнил часть требования #9843 Блок ошибок писать в БД

--- src/InforoomInternet/Global.asax.cs
+++ src/InforoomInternet/Global.asax.cs
@@ -29,6 +29,7 @@ namespace InforoomInternet
 		public Global()
 			: base(Assembly.Load("InforoomInternet"))
 		{
+			Logger = new HttpSessionLog(typeof(Global));
 			LibAssemblies.Add(Assembly.Load("Common.Web.Ui"));
 			//LibAssemblies.Add(Assembly.Load("InternetInterface"));
 			Logger.ErrorSubject = "Ошибка в IVRN";
