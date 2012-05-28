f691d1ce9b9df6ee265867e437d4f42192754ec4
Квасов Роман <r.kvasov@analit.net>
2012-05-28 10:25:41 +0400

    исрпавил ошибку при запуске приложения

--- src/InforoomInternet/Initializers/ActiveRecord.cs
+++ src/InforoomInternet/Initializers/ActiveRecord.cs
@@ -1,16 +1,12 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using Castle.ActiveRecord;
 using Castle.ActiveRecord.Framework;
-using Castle.ActiveRecord.Framework.Config;
-using Common.MySql;
 using Common.Web.Ui.ActiveRecordExtentions;
 using Common.Web.Ui.Models.Editor;
 using InternetInterface.Models;
 using NHibernate.Engine;
 using NHibernate.Type;
-using Environment=NHibernate.Cfg.Environment;
 
 namespace InforoomInternet.Initializers
 {
@@ -24,20 +20,7 @@ namespace InforoomInternet.Initializers
 
 		public override void Initialize(IConfigurationSource config)
 		{
-			var newConfig = new InPlaceConfigurationSource();
-			newConfig.IsRunningInWebApp = true;
-			newConfig.PluralizeTableNames = true;
-			newConfig.Add(typeof(ActiveRecordBase),
-				new Dictionary<string, string> {
-					{Environment.Dialect, "NHibernate.Dialect.MySQLDialect"},
-					{Environment.ConnectionDriver, "NHibernate.Driver.MySqlDataDriver"},
-					{Environment.ConnectionProvider, "NHibernate.Connection.DriverConnectionProvider"},
-					{Environment.ConnectionStringName, ConnectionHelper.GetConnectionName()},
-					{Environment.ProxyFactoryFactoryClass, "NHibernate.ByteCode.Castle.ProxyFactoryFactory, NHibernate.ByteCode.Castle"},
-					{Environment.Hbm2ddlKeyWords, "none"},
-				});
-
-			base.Initialize(newConfig);
+			base.Initialize(config);
 
 			SetupFilters();
 		}
--- src/InternetInterface/Initializers/ActiveRecord.cs
+++ src/InternetInterface/Initializers/ActiveRecord.cs
@@ -1,11 +1,5 @@
-﻿using System.Collections.Generic;
-using Castle.ActiveRecord;
-using Castle.ActiveRecord.Framework;
-using Castle.ActiveRecord.Framework.Config;
-using Common.MySql;
-using Common.Web.Ui.ActiveRecordExtentions;
+﻿using Common.Web.Ui.ActiveRecordExtentions;
 using Common.Web.Ui.MonoRailExtentions;
-using NHibernate.Cfg;
 
 namespace InternetInterface.Initializers
 {
@@ -15,24 +9,6 @@ namespace InternetInterface.Initializers
 		{
 			Assemblies = new [] { "InternetInterface" };
 			AdditionalTypes = new[] {typeof (ValidEventListner)};
-		}
-
-		public override void Initialize(IConfigurationSource config)
-		{
-			var newConfig = new InPlaceConfigurationSource();
-			newConfig.IsRunningInWebApp = true;
-			newConfig.PluralizeTableNames = true;
-			newConfig.Add(typeof(ActiveRecordBase),
-				new Dictionary<string, string> {
-					{Environment.Dialect, "NHibernate.Dialect.MySQLDialect"},
-					{Environment.ConnectionDriver, "NHibernate.Driver.MySqlDataDriver"},
-					{Environment.ConnectionProvider, "NHibernate.Connection.DriverConnectionProvider"},
-					{Environment.ConnectionStringName, ConnectionHelper.GetConnectionName()},
-					{Environment.ProxyFactoryFactoryClass, "NHibernate.ByteCode.Castle.ProxyFactoryFactory, NHibernate.ByteCode.Castle"},
-					{Environment.Hbm2ddlKeyWords, "none"},
-				});
-
-			base.Initialize(newConfig);
 		}
 	}
 }
