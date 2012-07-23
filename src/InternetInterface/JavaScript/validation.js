60842184d49a5837e5ae100a3fb2243427342ea2
Золотарев Александр <a.zolotarev@analit.net>
2012-07-23 19:12:39 +0400

    Исправил ошибку в комментарии

--- src/InternetInterface/JavaScript/validation.js
+++ src/InternetInterface/JavaScript/validation.js
@@ -1,15 +1,33 @@
 ﻿function BackPaymentValidation() {
 	$('.backPayment').validate({
-		errorElement: "div",
-		errorLabelContainer: "#errorContainer",
-		submitHandler: function (form) {
-			var comment = $('#paymentReason').val();
-			if (comment != "") {
-				form.action += ("?comment=" + comment);
-				form.submit();
-			} else {
-				$('#paymentReasonMessage').css("display", "inline");
+		errorLabelContainer: "#paymentReasonMessage",
+		ignore: "",
+		rules: {
+			paymentReason: {
+				required: true
 			}
-		}
+		},
+		messages:
+			{
+				paymentReason: {
+					required: "Введите комментарий"
+				}
+			}
 	});
+}
+
+function validateForm(form) {
+		var comment = $('#paymentReason').val();
+		if (comment != "") {
+			form.action += ("?comment=" + comment);
+			form.submit();
+			return true;
+		} else {
+			$('#paymentReasonMessage').css("display", "inline");
+			return false;
+	}
+	}
+
+function changeCommentText(field) {
+	$('.hiddenCommentText').val($(field).val());
 }
