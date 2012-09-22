85389cecbf41c1f342eae28657910b29f2fbf071
Золотарев Александр <a.zolotarev@analit.net>
2012-09-22 18:11:14 +0400

    Выполнил комментарий Романа к #7753

---
+++ src/InternetInterface.Test/Integration/ClientFixture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InternetInterface.Models;
+using InternetInterface.Test.Helpers;
+using NUnit.Framework;
+using Test.Support;
+
+namespace InternetInterface.Test.Integration
+{
+	[TestFixture]
+	public class ClientFixture : IntegrationFixture
+	{
+		[Test]
+		public void Ger_write_offs_test()
+		{
+			var client = ClientHelper.Client();
+			session.SaveOrUpdate(client);
+			var writeOff = new WriteOff(client, 500m);
+			var userWriteOff = new UserWriteOff(client, 100m, "testUserWriteOff");
+			session.Save(writeOff);
+			session.Save(userWriteOff);
+			Flush();
+			var writeOffs = client.GetWriteOffs(session, string.Empty);
+			Assert.AreEqual(writeOffs.Count, 2);
+			Assert.That(writeOffs[0].WriteOffSum, Is.EqualTo(500m));
+			Assert.That(writeOffs[1].WriteOffSum, Is.EqualTo(100m));
+			Assert.That(writeOffs[1].Comment, Is.EqualTo("testUserWriteOff"));
+		}
+	}
+}
