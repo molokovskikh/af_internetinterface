ebe3b77a441134fe11870075c1968e98df39341e
Золотарев Александр <a.zolotarev@analit.net>
2012-10-05 15:41:25 +0400

    Чиню тест #12251

--- src/InternetInterface/Mails/LawyerUserWriteOffNotice.cs
+++ src/InternetInterface/Mails/LawyerUserWriteOffNotice.cs
@@ -15,7 +15,8 @@ namespace InternetInterface.Mails
 			var messageText = new StringBuilder();
 			messageText.AppendLine("Зарегистрировано разовое списание для Юр.Лица.");
 			messageText.AppendFormat("Клиент: {0} - {1} ({2})\r\n", writeOff.Client.Id.ToString("00000"), writeOff.Client.LawyerPerson.Name, writeOff.Client.Name);
-			messageText.AppendFormat("Списание: Сумма - {0}, Комментарий: {1}, Оператор: {2}", writeOff.Sum.ToString("0.00"), writeOff.Comment, writeOff.Registrator.Name);
+			var registrator = writeOff.Registrator != null ? writeOff.Registrator.Name : string.Empty;
+			messageText.AppendFormat("Списание: Сумма - {0}, Комментарий: {1}, Оператор: {2}", writeOff.Sum.ToString("0.00"), writeOff.Comment, registrator);
 			Email.Send(messageText, "Списание для Юр.Лица.", "InternetBilling@analit.net");
 		}
 	}
