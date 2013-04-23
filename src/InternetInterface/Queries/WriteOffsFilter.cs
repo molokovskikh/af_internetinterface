8d1f9b3ca567a19429752933b8e6e0ed6a54dc2e
Золотарев Александр <a.zolotarev@analit.net>
2013-04-23 09:26:29 +0400

    Работаю по теребованию #9787

---
+++ src/InternetInterface/Queries/WriteOffsFilter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Common.Web.Ui.Helpers;
+using InternetInterface.Models;
+using NHibernate;
+using NHibernate.Linq;
+
+namespace InternetInterface.Queries
+{
+	public class WriteOffsFilter : PaginableSortable
+	{
+		public ISession Session { get; set; }
+		public RegionHouse Region { get; set; }
+		public DateTime BeginDate { get; set; }
+		public DateTime EndDate { get; set; }
+
+		public IList<BaseItemForTable> Find()
+		{
+			var firstDataQuery = Session.Query<WriteOff>().Where(w => w.WriteOffDate >= BeginDate.Date && w.WriteOffDate <= EndDate.Date);
+		}
+	}
+}
