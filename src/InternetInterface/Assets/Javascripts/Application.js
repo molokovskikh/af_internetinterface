57159cd007d87675d34ffdb43013f972cfb4383f
Золотарев Александр <a.zolotarev@analit.net>
2013-05-26 17:00:41 +0400

    Выполнил #17245 поиск по дате на странице заявки

--- src/InternetInterface/Assets/Javascripts/Application.js
+++ src/InternetInterface/Assets/Javascripts/Application.js
@@ -5,6 +5,6 @@
 	
 	Date.format = 'dd.mm.yyyy';
 	$('.graph_date').datepicker();
-	$('.date-pick').datepicker();
+	$('.date-pick').datepicker({ changeYear: true });
 	$('.date_field').datepicker();
 });
