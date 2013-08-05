c66804e396aefdd9ba0e575ec394d7f5436bfa96
Золотарев Александр <a.zolotarev@analit.net>
2013-08-05 20:10:23 +0400

    Поправил миграцию по #18731

--- migrations/948_00_updateStartNoBlockTime.sql
+++ migrations/948_00_updateStartNoBlockTime.sql
@@ -7,7 +7,7 @@ clientid INT unsigned) engine=MEMORY ;
 insert into internet.StartNoBlockUpdate (NewStartNoBlock, clientid)
 SELECT max(logtime) as NewStartNoBlock, cl.id FROM `logs`.ClientInternetLogs C
 join internet.clients cl on cl.id = c.clientid
-where c.disabled = true and cl.startNoBlock < c.LogTime and cl.disabled = false
+where c.disabled = false and cl.startNoBlock < c.LogTime and cl.disabled = false
 group by cl.id;
 
 update internet.Clients c
