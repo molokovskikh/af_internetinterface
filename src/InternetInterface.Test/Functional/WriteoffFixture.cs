cc68c2f3e60a32dbcb0899a57de7ecd58b251ab4
Квасов Роман <r.kvasov@analit.net>
2014-04-23 15:40:13 +0400

    Починил страницу списания

---
+++ src/InternetInterface.Test/Functional/WriteoffFixture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using Headless;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Test.Support.Web;
+
+namespace InternetInterface.Test.Functional
+{
+	[TestFixture]
+	public class WriteoffFixture
+	{
+		[Test]
+		public void View_write_off()
+		{
+			var browser = new Browser();
+			var page = browser.GoTo<DynamicHtmlPage>(new Uri(WatinFixture2.GetUri("Map/SiteMap")));
+			var link = page.Find<HtmlLink>().All().First(l => l.Text == "Списания");
+			page = link.Click();
+			Assert.That(page.Html, Is.StringContaining("Имя клиента"));
+		}
+	}
+}
