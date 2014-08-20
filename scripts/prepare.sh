6b86fe0884c7ebf883303d245b8fd69f70162199
Квасов Роман <r.kvasov@analit.net>
2014-08-20 14:19:15 +0400

    Исправил тестовое развертывание

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -2,5 +2,6 @@
 
 npm install
 bower install
-bake packages:install
-bake generate:assembly:info
+bake packages:install | iconv -s -f cp866 -t cp1251
+bake generate:assembly:info | iconv -s -f cp866 -t cp1251
+bake fix:packages | iconv -s -f cp866 -t cp1251
