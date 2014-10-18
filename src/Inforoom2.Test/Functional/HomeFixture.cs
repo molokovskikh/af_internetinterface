cf4edeccdede759fd76ebcf56ae28bc43e948bcd
Сарычев Алексей <asarychev@analit.net>
2014-10-18 10:41:56 +0400

    Добавил тесты для смены города и страницы вопросы и ответы

---
+++ src/Inforoom2.Test/Functional/HomeFixture.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Web.UI.WebControls;
+using Inforoom2.Helpers;
+using Inforoom2.Models;
+using NHibernate.Linq;
+using NUnit.Framework;
+
+namespace Inforoom2.Test.Functional
+{
+	[TestFixture]
+	public class HomeFixture : BaseFixture
+	{
+		protected Question Question;
+
+		[Test, Description("Проверка определения города")]
+		public void CitySelectTest()
+		{	
+			Open();
+			AssertText("Ваш город");
+			var link = browser.FindElementByCssSelector("#cityLink");
+			Assert.That(link.Text, Is.EqualTo("Воронеж"));
+			link.Click();
+			WaitForVisibleCss(".selectCity");
+			var cityLink = browser.FindElementByCssSelector(".selectCity");
+			cityLink.Click();
+			var cities = browser.FindElementsByCssSelector(".city");
+			foreach(var city in cities)
+				if(city.Text == "Борисоглебск")
+					city.Click();
+			Open("Faq");
+			AssertText("Ваш город");
+			link = browser.FindElementByCssSelector("#cityLink");
+			Assert.That(link.Text, Is.EqualTo("Борисоглебск"));
+		}
+
+	}
+}
--- src/Inforoom2/Models/BaseModel.cs
+++ src/Inforoom2/Models/BaseModel.cs
@@ -1,4 +1,11 @@
-﻿using NHibernate.Mapping.Attributes;
+﻿using System.Linq;
+using System.Web.Services.Configuration;
+using Inforoom2.Helpers;
+using NHibernate;
+using NHibernate.Linq;
+using NHibernate.Mapping.Attributes;
+using NHibernate.Util;
+using NHibernate.Validator.Cfg.Loquacious.Impl;
 
 namespace Inforoom2.Models
 {
@@ -10,5 +17,16 @@ namespace Inforoom2.Models
 		[Id(0,Name = "Id")]
 		[Generator(1,Class = "native")]
 		public virtual int Id { get; set; }
+
+		public virtual int GetNextPriority(ISession session)
+		{
+			var classAttr = GetType().GetCustomAttributes(typeof(ClassAttribute), false).First() as ClassAttribute;
+			var tablename = new TableNamingStrategy().TableName(classAttr.Table);
+			var query = session.CreateSQLQuery("SELECT MAX(priority) AS max FROM "+tablename);
+			var result = query.List();
+			if (result.First() != null)
+				return (int)result.First() + 1;
+			return 1;
+		}
 	}
 }
