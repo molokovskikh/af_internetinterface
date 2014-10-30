f26b989d7e76bac41e6d269eafb6ed2fbb17f14d
Сарычев Алексей <a.sareychev@analit.net>
2014-10-30 18:10:54 +0400

    #23931 улучшенная фильтрация для выгрузки клиентов в поиске

--- src/InternetInterface/Models/LawyerPerson.cs
+++ src/InternetInterface/Models/LawyerPerson.cs
@@ -134,5 +134,62 @@ namespace InternetInterface.Models
 			PeriodEnd = PeriodEnd.AddMonths(1).LastDayOfMonth();
 			return results.Where(w => w.Sum > 0).ToList();
 		}
+
+		public virtual Dictionary<string,string> ParseAddress()
+		{
+			var obj = new Dictionary<string,string>();
+			obj["City"] = "--";
+			obj["Street"] = "--";
+			obj["House"] = "--";
+			obj["Apartment"] = "--";
+			return obj;
+			//Можно парсить адреса юриков но будут ошибки и результат в районе 70%
+			IList<string> actual = ActualAdress == null ? new string[0] : ActualAdress.Split(',');
+			IList<string> paper = LawyerAdress == null ? new string[0] : LawyerAdress.Split(',');
+			if(actual.Count == 0)
+				return obj;
+			if(actual.Count == 1)
+			{
+				obj["City"] = actual[0];
+				return obj;
+			}
+			int digital;
+			int.TryParse(actual[0], out digital);
+			bool officeFlag = actual[actual.Count -1].Contains("оф");
+			//Стандартный адрес
+			if(actual.Count == 3 && !officeFlag)
+			{
+				obj["City"] = actual[0];
+				obj["Street"] = actual[1];
+				obj["House"] = actual[2];
+			}
+
+			if(actual.Count == 3 && officeFlag)
+			{
+				obj["Street"] = actual[0];
+				obj["House"] = actual[1];
+			}
+			//индекс+стандартный адрес
+			if(digital != 0 && actual.Count == 4)
+			{
+				obj["City"] = actual[1];
+				obj["Street"] = actual[2];
+				obj["House"] = actual[3];
+			}
+			//индекс+область+стандартный адрес
+			if(digital != 0 && actual.Count == 5 && actual[1].Contains("обл"))
+			{
+				obj["City"] = actual[2];
+				obj["Street"] = actual[3];
+				obj["House"] = actual[4];
+			}
+			if(actual.Count == 2)
+			{
+				obj["Street"] = actual[0];
+				obj["House"] = actual[1];
+			}
+			return obj;
+		}
+
 	}
 }
