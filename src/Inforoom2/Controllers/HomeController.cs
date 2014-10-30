8e4508499df0e972c768cdc04f47c5e0ba205643
Евгений Федоров <efedorov@analit.net>
2014-10-30 18:50:39 +0400

    #28981 Подготовил проект и инфраструктуру к автоматическому развертыванию

--- src/Inforoom2/Controllers/HomeController.cs
+++ src/Inforoom2/Controllers/HomeController.cs
@@ -10,21 +10,7 @@ namespace Inforoom2.Controllers
 	{
 		public ActionResult Index()
 		{
-			var session = NHibernateActionFilter.sessionFactory.OpenSession();
-			var h = session.Get<House>(28);
-			session.BeginTransaction();
-			h.Number = "I defgfdgdg";
-			session.Flush();
-			session.Transaction.Commit();
-			session.Save(h);
-			session.Flush();
-			
-
 			ViewBag.Message = "HomeController";
-			if (User != null && User.HasPermissions("CanEverything")) {
-				ViewBag.Message = "Я одмин";
-			}
-
 			var news = DbSession.Query<NewsBlock>().Where(k => k.IsPublished).ToList();
 			ViewBag.News = news;
 			
