e9a992860410d875a46fa7416b6c42dc8768840b
Сарычев Алексей <asarychev@analit.net>
2014-12-01 15:10:20 +0400

    Тестирование скорости  speedtest

---
+++ src/Inforoom2/Controllers/TestSpeedController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Inforoom2.Models;
+using NHibernate.Linq;
+
+namespace Inforoom2.Controllers
+{
+	/// <summary>
+	/// Отображает страницы оферты
+	/// </summary>
+	public class TestSpeedController : BaseController
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
+}
