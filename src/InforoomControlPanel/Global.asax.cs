e60b056603af8a20801853193c645216fc928e4d
Федоров Евгений <efedorov@analit.net>
2014-12-08 16:41:14 +0400

    #29781 совместная авторизация inforoom control panel и internetinterface

--- src/InforoomControlPanel/Global.asax.cs
+++ src/InforoomControlPanel/Global.asax.cs
@@ -18,12 +18,28 @@ namespace InforoomControlPanel
 {
 	public class MvcApplication : System.Web.HttpApplication
 	{
-		
+			private static ISessionFactory _sessionFactory;
+
+		public static ISessionFactory SessionFactory
+		{
+			get
+			{
+				if (_sessionFactory != null) {
+					return _sessionFactory;
+				}
+				else {
+					InitializeSessionFactory();
+					return _sessionFactory;
+				}
+			}
+			private set { _sessionFactory = value; }
+		}
+
 		protected void Application_Start()
 		{
 			AreaRegistration.RegisterAllAreas();
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
-			Inforoom2.MvcApplication.InitializeSessionFactory();
+		//	InitializeSessionFactory();
 		}
 
 		protected void Application_PostAuthenticateRequest(Object sender, EventArgs e)
@@ -43,6 +59,33 @@ namespace InforoomControlPanel
 				}
 			}
 		}
-		
+
+	public static void InitializeSessionFactory()
+		{
+			Configuration configuration = new Configuration();
+			configuration.SetNamingStrategy(new TableNamingStrategy());
+			var nhibernateConnectionString = ConfigurationManager.AppSettings["nhibernateConnectionString"];
+			configuration.SetProperty("connection.provider", "NHibernate.Connection.DriverConnectionProvider")
+				.SetProperty("connection.driver_class", "NHibernate.Driver.MySqlDataDriver")
+				.SetProperty("connection.connection_string", nhibernateConnectionString)
+				.SetProperty("dialect", "NHibernate.Dialect.MySQL5Dialect")
+				.SetProperty("current_session_context_class", "web");
+
+			/*	var listener = new SyncObject();			configuration.EventListeners.PostUpdateEventListeners =
+				new IPostUpdateEventListener[] { listener };
+			configuration.EventListeners.PostInsertEventListeners =
+				new IPostInsertEventListener[] { listener };*/
+
+			/*	var configurationPath = HttpContext.Current.Server.MapPath(@"~\Nhibernate\hibernate.cfg.xml");
+			configuration.Configure(configurationPath);*/
+			configuration.AddInputStream(
+				NHibernate.Mapping.Attributes.HbmSerializer.Default.Serialize(Assembly.GetAssembly(typeof(Client))));
+
+			//TODO Раскоментировать для создания таблиц с помощью Nhibernate
+			/*	var schema = new NHibernate.Tool.hbm2ddl.SchemaExport(configuration);
+			schema.Create(false, true);*/
+			SessionFactory = configuration.BuildSessionFactory();
+		}
+
 	}
 }
