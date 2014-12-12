22767ff65f1923e44106d74731ed843e1ceefcdf
Федоров Евгений <efedorov@analit.net>
2014-12-12 19:28:51 +0400

    подтвреждение смены тарифа

---
+++ src/Inforoom2/Scripts/Tariffs.js
@@ -0,0 +1,7 @@
+﻿$(".connectfee").on("click", function() {
+	cli.areYouSure("Вы уверены, что хотите сменить тариф?", function() {
+		$('#changeplan').submit();
+	}, "Подтвердите действие");
+
+	return false;
+})
