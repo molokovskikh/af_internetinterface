f91ddc2cc4a81ba1b889f3ae88ecb8c2e90fa9f6
Сарычев Алексей <asarychev@analit.net>
2015-01-26 21:10:22 +0400

    31177 Не работает функция ПОДКЛЮЧЕН ЛИ ВАШ ДОМ?

--- src/Inforoom2/Controllers/AboutController.cs
+++ src/Inforoom2/Controllers/AboutController.cs
@@ -13,18 +13,38 @@ namespace Inforoom2.Controllers
 	/// </summary>
 	public class AboutController : BaseController
 	{
+		/// <summary>
+		/// Страница о компании
+		/// </summary>
 		public ActionResult Index()
 		{
 			return View();
 		}
 
+		/// <summary>
+		/// Реквизиты компании
+		/// </summary>
 		public ActionResult Details()
 		{
 			return View();
 		}
 
+		/// <summary>
+		/// Страница оплаты, если пользователь не авторизован
+		/// </summary>
 		public ActionResult Payment()
 		{
+			return View();
+		}
+
+		/// <summary>
+		/// Списки подключенных домов
+		/// </summary>
+		public ActionResult ConnectedHousesLists()
+		{
+			var addresses = DbSession.Query<SwitchAddress>().Where(i=>i.House != null).OrderBy(i=>i.House.Street.Name).ToList();
+
+			ViewBag.Addresses = addresses;
 			return View();
 		}
 	}
