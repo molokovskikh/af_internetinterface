e690886d9eb6f6221ae7f23c7e338757e6b53622
Андрей Гераськин <fcverona@gmail.com>
2015-03-31 11:47:47 +0300

    i32957 Исправление ощибки с невозможностью активации "Обещанного платежа"

--- src/Inforoom2/Models/Services/DeferredPayment.cs
+++ src/Inforoom2/Models/Services/DeferredPayment.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using Common.Tools;
+using Inforoom2.Helpers;
 using NHibernate;
 using NHibernate.Mapping.Attributes;
 
@@ -23,30 +24,41 @@ namespace Inforoom2.Models.Services
 			}
 		}
 
-		// Время до момента, когда клиенту станет доступен "Обещанный платеж" (заполняется при вызове метода IsAvailableInThisTime)
-		public virtual TimeSpan TimeToActivation { get; protected set; }
+		// Причина, по которой клиенту не доступен "Обещанный платеж" (заполняется при вызове метода IsAvailableInThisTime)
+		public virtual string NotActivateReason { get; protected set; }
 
 		// Метод для проверки, доступен ли клиенту "Обещанный платеж" в данный момент времени
 		public virtual bool IsAvailableInThisTime(Client client)
 		{
-			TimeToActivation = TimeSpan.Zero;
+			NotActivateReason = "";
 			var lastService = client.ClientServices.OrderBy(cs => cs.BeginDate).LastOrDefault(s => s.Service.Id == Id);
-			if (lastService == null)							// Т.е. "Обещанный платеж" ещё ни разу не активировался
+			if (lastService == null)                   // Т.е. "Обещанный платеж" ещё ни разу не активировался
 				return true;
-			if (lastService.IsActivated)
+			if (lastService.IsActivated) {
+				NotActivateReason = "Данная услуга у вас уже активирована.";
 				return false;
+			}
 
 			var serviceDate = lastService.BeginDate ?? new DateTime();
 			// Проверка, запрещающая клиенту повторно подключить услугу без пополнения баланса на 80% от цены тарифа (СОГЛАСОВАНО)
 			if (serviceDate != DateTime.MinValue) {
-				var clientPayments = (client.Payments != null) ? client.Payments.Where(p => p.PaidOn > serviceDate).ToList() : null;
-					var paySum = (clientPayments != null) ? clientPayments.Sum(p => p.Sum) : 0;
-				if (paySum < (0.8m * client.Plan.Price))
+				var clientPayments = 
+						(client.Payments != null) ? client.Payments.Where(p => p.PaidOn.Date >= serviceDate.Date).ToList() : null;
+				var paySum = (clientPayments != null) ? clientPayments.Sum(p => p.Sum) : 0;
+				var minSum = 0.8m * client.Plan.Price;
+				if (paySum < minSum) {
+					NotActivateReason = string.Format("С последнего подключения услуги от {0} не пополнялся баланс на сумму абонентской платы тарифа.",
+							serviceDate.Date.ToShortDateString());
 					return false;
+				}
 			}
 
-			TimeToActivation = serviceDate.AddDays(30) - DateTime.Now;
-			return (TimeToActivation < TimeSpan.Zero);
+			var timeToActivation = serviceDate.AddDays(30) - DateTime.Now;
+			if (timeToActivation > TimeSpan.Zero) {
+				NotActivateReason = "Услуга станет доступна через " + StringHepler.ShowTimeLeft(timeToActivation);
+				return false;
+			}
+			return true;
 		}
 
 		public override bool IsActivableFor(Client client)
