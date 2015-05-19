f128ca54e9e29360dc5f1a5534a1001ecd993d84
Сарычев Алексей <asarychev@analit.net>
2015-05-19 15:47:17 +0300

    34139 фикс отправки сообщений об ошибках об смс поставщике

--- src/InternetInterface.Background/SendSmsNotification.cs
+++ src/InternetInterface.Background/SendSmsNotification.cs
@@ -23,11 +23,14 @@ namespace InternetInterface.Background
 
 		protected override void Process()
 		{
-			SendMessages();
+			//Отправка смс отключена 19.05.2015, так как валятся ошибки в сервис из-за того, что мы разорвали отношения с нашим поставщиком смс
+			//todo вернуть отправку смс, когда будет поставщик
+			//SendMessages();
 		}
 
 		public List<SmsMessage> SendMessages()
 		{
+			
 			var messages = Session.Query<SmsMessage>().Where(m =>
 				!m.IsSended
 					&& m.PhoneNumber != null
