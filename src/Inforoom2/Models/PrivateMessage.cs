e99b8d07ab56a3e737117fc05aad5da682aab779
Андрей Гераськин <fcverona@gmail.com>
2015-05-21 17:19:35 +0300

    i34001 Функционал для работы с приватным сообщением в новом интерфейсе

---
+++ src/Inforoom2/Models/PrivateMessage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using NHibernate.Mapping.Attributes;
+using NHibernate.Validator.Constraints;
+
+namespace Inforoom2.Models
+{
+	[Class(0, Table = "MessagesForClients", Schema = "internet", NameType = typeof(PrivateMessage))]
+	public class PrivateMessage : BaseModel
+	{
+		[Property, Description("Текст сообщения"), NotNullNotEmpty(Message = "Введите сообщение")]
+		public virtual string Text { get; set; }
+
+		[Property(NotNull = true), Description("Показывать")]
+		public virtual bool Enabled { get; set; }
+
+		[Property(NotNull = true), Description("Дата регистрации сообщения")]
+		public virtual DateTime RegDate { get; set; }
+
+		[Property(NotNull = true), Description("Дата отключения сообщения со страницы клиента")]
+		public virtual DateTime EndDate { get; set; }
+
+		[ManyToOne]
+		public virtual Client Client { get; set; }
+
+		[ManyToOne]
+		public virtual Employee Registrator { get; set; }
+	}
+}
