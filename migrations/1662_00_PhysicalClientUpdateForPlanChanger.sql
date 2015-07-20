7779500e2cd839970569643fcb7cbdef2f8108ca
Якименко Александр <ayakimenko@analit.net>
2015-07-20 17:32:10 +0300

    i32805 Обновление даты смены тарифа у физ лиц, связанное с planChanger

---
+++ migrations/1662_00_PhysicalClientUpdateForPlanChanger.sql
@@ -0,0 +1,8 @@
+﻿use internet;
+UPDATE internet.physicalclients as p
+INNER JOIN  internet.tariffs AS t ON p.Tariff = t.Id  
+INNER JOIN  internet.clients AS c ON p.Id = c.PhysicalClient
+ SET p._LastTimePlanChanged = c.RegDate
+ WHERE
+ p._LastTimePlanChanged = '0001-01-01' 
+ AND p.tariff = 85
