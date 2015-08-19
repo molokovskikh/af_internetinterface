bec049a325a457686697d4e93f515b117459cf16
Якименко Александр <ayakimenko@analit.net>
2015-08-19 13:17:41 +0300

    i35183 Дополнение к задаче, небольшие изменения

--- src/Inforoom2/Models/ClientRentalHardware.cs
+++ src/Inforoom2/Models/ClientRentalHardware.cs
@@ -3,19 +3,17 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using Common.Tools;
-using Inforoom2.Components;
 using Inforoom2.Helpers;
 using NHibernate;
-using NHibernate.Linq;
 using NHibernate.Mapping.Attributes;
 using NHibernate.Validator.Constraints;
 
 namespace Inforoom2.Models
 {
 	/// <summary>
-	/// Модель услуги "Аренда оборудования" (по сути это Аренда или арендуемое оборудование)
+	///     Модель услуги "Аренда оборудования" (по сути это Аренда или арендуемое оборудование)
 	/// </summary>
-	[Class(0, Table = "ClientRentalHardware", NameType = typeof(ClientRentalHardware))]
+	[Class(0, Table = "ClientRentalHardware", NameType = typeof (ClientRentalHardware))]
 	public class ClientRentalHardware : BaseModel
 	{
 		public ClientRentalHardware()
@@ -64,8 +62,8 @@ namespace Inforoom2.Models
 		public virtual string DeactivateComment { get; set; }
 
 		[Bag(0, Table = "ClientHardwareParts", Cascade = "all-delete-orphan")]
-		[NHibernate.Mapping.Attributes.Key(1, Column = "ClientRentId")]
-		[OneToMany(2, ClassType = typeof(ClientHardwarePart)), Description("Комплектация")]
+		[Key(1, Column = "ClientRentId")]
+		[OneToMany(2, ClassType = typeof (ClientHardwarePart)), Description("Комплектация")]
 		public virtual IList<ClientHardwarePart> ClientHardwareParts { get; set; }
 
 		// Метод активации "Аренды оборудования"
@@ -76,15 +74,18 @@ namespace Inforoom2.Models
 			Employee = employee;
 
 			// 1-ое обращение - об активации услуги
-			var message = String.Format("Услуга \"Аренда оборудования типа \"{0}\" активирована", Hardware.Name);
-			var appeal = new Appeal(message, Client, AppealType.User) {
+			var message = string.Format("Услуга \"Аренда оборудования типа \"{0}\" активирована", Hardware.Name);
+			var appeal = new Appeal(message, Client, AppealType.User)
+			{
 				Employee = employee
 			};
 			dbSession.Save(appeal);
 
 			// 2-ое обращение - об арендуемом оборудовании
-			message = string.Format("Клиент арендовал \"{0}\", модель \"{1}\", S/N {2}", Hardware.Name, Name, SerialNumber);
-			appeal = new Appeal(message, Client, AppealType.User) {
+			message = string.Format("Клиент арендовал \"{0}\", модель \"{1}\", S/N {2}.<br/>Выдан {3}", Hardware.Name, Name,
+				SerialNumber, GetAbsentPartsOfRentedHardware(true));
+			appeal = new Appeal(message, Client, AppealType.User)
+			{
 				Employee = employee
 			};
 			dbSession.Save(appeal);
@@ -97,9 +98,12 @@ namespace Inforoom2.Models
 			IsActive = false;
 			dbSession.Update(this);
 			//сообщенин о деактивации
-			var message = String.Format("Услуга \"Аренда оборудования типа \"{0}\" (<a href='{1}/HardwareRent/UpdateHardwareRent/{2}'>{2}</a>) деактивирована,<br/> модель \"{3}\", S/N {4}.<br/>Возвращен {5}",
-				Hardware.Name, ConfigHelper.GetParam("adminPanelNew"), Id, Name, SerialNumber, GetAbsentPartsOfRentedHardware());
-			var appeal = new Appeal(message, Client, AppealType.User) {
+			var message =
+				string.Format(
+					"Услуга \"Аренда оборудования типа \"{0}\" (<a href='{1}/HardwareRent/UpdateHardwareRent/{2}'>{2}</a>) деактивирована,<br/> модель \"{3}\", S/N {4}.<br/>Возвращен {5}",
+					Hardware.Name, ConfigHelper.GetParam("adminPanelNew"), Id, Name, SerialNumber, GetAbsentPartsOfRentedHardware());
+			var appeal = new Appeal(message, Client, AppealType.User)
+			{
 				Employee = employee
 			};
 			dbSession.Save(appeal);
@@ -108,12 +112,15 @@ namespace Inforoom2.Models
 		//Комментарий к деактивации "Аренды оборудования"
 		public virtual void DeactivateCommentSend(ISession dbSession, Employee employee)
 		{
-			string deactivateAppeal = DeactivateComment;
+			var deactivateAppeal = DeactivateComment;
 			if (!string.IsNullOrEmpty(deactivateAppeal)) {
-				deactivateAppeal = String.Format("Комментарий к деактивации аренды \"{0}\" (<a href='{1}/HardwareRent/UpdateHardwareRent/{2}'>{2}</a>) : {3}",
-					Hardware.Name, ConfigHelper.GetParam("adminPanelNew"), Id, deactivateAppeal);
+				deactivateAppeal =
+					string.Format(
+						"Комментарий к деактивации аренды \"{0}\" (<a href='{1}/HardwareRent/UpdateHardwareRent/{2}'>{2}</a>) : {3}",
+						Hardware.Name, ConfigHelper.GetParam("adminPanelNew"), Id, deactivateAppeal);
 
-				var appealWithComment = new Appeal(deactivateAppeal, Client, AppealType.User) {
+				var appealWithComment = new Appeal(deactivateAppeal, Client, AppealType.User)
+				{
 					Employee = employee
 				};
 				dbSession.Save(appealWithComment);
@@ -123,21 +130,25 @@ namespace Inforoom2.Models
 		//Комментарий к деактивации "Аренды оборудования", после его отправки в архив (чтобы контролировать задолжность)
 		public virtual void DeactivateRentUpdateAppeal(ISession dbSession, Employee employee)
 		{
-			var message = String.Format("Обновление деактивированного оборудования \"{0}\" (<a href='{1}/HardwareRent/UpdateHardwareRent/{2}'>{2}</a>).<br/>Возвращен {3}",
-				Hardware.Name, ConfigHelper.GetParam("adminPanelNew"), Id, GetAbsentPartsOfRentedHardware());
+			var message =
+				string.Format(
+					"Обновление деактивированного оборудования \"{0}\" (<a href='{1}/HardwareRent/UpdateHardwareRent/{2}'>{2}</a>).<br/>Возвращен {3}",
+					Hardware.Name, ConfigHelper.GetParam("adminPanelNew"), Id, GetAbsentPartsOfRentedHardware());
 
 			if (DeactivateComment != "") {
-				message = String.Format("{0}<br/>Комментарий к деактивации аренды \"{1}\" : {2}", message, Hardware.Name, DeactivateComment);
+				message = string.Format("{0}<br/>Комментарий к деактивации аренды \"{1}\" : {2}", message, Hardware.Name,
+					DeactivateComment);
 			}
 			// 1-ое обращение - об активации услуги
-			var appeal = new Appeal(message, Client, AppealType.User) {
+			var appeal = new Appeal(message, Client, AppealType.User)
+			{
 				Employee = employee
 			};
 			dbSession.Save(appeal);
 		}
 
 		/// <summary>
-		/// Возвращает итоговую цену за месяц аренды оборудования 
+		///     Возвращает итоговую цену за месяц аренды оборудования
 		/// </summary>
 		public virtual decimal GetPrice()
 		{
@@ -145,19 +156,24 @@ namespace Inforoom2.Models
 		}
 
 		/// <summary>
-		/// Получение всей комплектации оборудования, допоолнение его, если комплектация пополнилась, 
-		/// если у комплектации на форме не назначена аренда, назначается теккущей аренде (это при создании, т.к. на форме нет Id создаваемой аренды)
-		/// </summary> 
+		///     Получение всей комплектации оборудования, допоолнение его, если комплектация пополнилась,
+		///     если у комплектации на форме не назначена аренда, назначается теккущей аренде (это при создании, т.к. на форме нет
+		///     Id создаваемой аренды)
+		/// </summary>
 		public virtual void GetClientHardwareParts()
 		{
 			//если комплектация не пополнилась
 			if (ClientHardwareParts.Count == Hardware.HardwareParts.Count) {
 				//проверяем значение аренды, если аренда не указана
-				var clientHardwareParts = ClientHardwareParts.Where(s => s.ClientRentalHardware == null || s.ClientRentalHardware.Id == 0).ToList();
+				var clientHardwareParts =
+					ClientHardwareParts.Where(s => s.ClientRentalHardware == null || s.ClientRentalHardware.Id == 0).ToList();
 				//задаем текущую каждому эл-ту аренды
 				if (clientHardwareParts.Count > 0) {
-					ClientHardwareParts = clientHardwareParts.Select(o => {
-						o.ClientRentalHardware = o.ClientRentalHardware == null || o.ClientRentalHardware.Id == 0 ? this : o.ClientRentalHardware;
+					ClientHardwareParts = clientHardwareParts.Select(o =>
+					{
+						o.ClientRentalHardware = o.ClientRentalHardware == null || o.ClientRentalHardware.Id == 0
+							? this
+							: o.ClientRentalHardware;
 						return o;
 					}).ToList();
 				}
@@ -165,7 +181,7 @@ namespace Inforoom2.Models
 			else {
 				//если комплектация пополнилась
 				var newParts = Hardware.HardwareParts.Where(s => !ClientHardwareParts.Any(o => o.Part.Id == s.Id))
-					.Select(s => { return new ClientHardwarePart() { ClientRentalHardware = this, Part = s }; }).ToList();
+					.Select(s => { return new ClientHardwarePart {ClientRentalHardware = this, Part = s}; }).ToList();
 				//нужно добавить недостающие эл-ты в список арендуемых
 				if (newParts.Count > 0) {
 					ClientHardwareParts.AddEach(newParts);
@@ -174,16 +190,29 @@ namespace Inforoom2.Models
 		}
 
 		/// <summary>
-		/// Создание сообщения о списке недостающих эл-тов комплектации по аренде клиента
+		///     Создание сообщения о списке недостающих эл-тов комплектации по аренде клиента
 		/// </summary>
+		/// <param name="notGiven">Получить не выданные (по умолчанию не полученные)</param>
 		/// <returns>списке недостающих эл-тов комплектации</returns>
-		public virtual string GetAbsentPartsOfRentedHardware()
+		public virtual string GetAbsentPartsOfRentedHardware(bool notGiven = false)
 		{
-			var clientHardware = ClientHardwareParts.Count(s => !s.Absent) == Hardware.HardwareParts.Count || ClientHardwareParts.Count == 0
-				? "<strong>полный комплект</strong>"
-				: "<strong style='color:red;'>неполный комплект</strong>, отсуствует:</br>" +
-				  String.Join(",</br>", ClientHardwareParts.Where(s => s.Absent).Select(o => "- " + o.Part.Name).ToList());
-			return clientHardware;
+			if (notGiven) {
+				//возврат не выданных клиенту
+				var clientHardware = ClientHardwareParts.Count(s => !s.NotGiven) == Hardware.HardwareParts.Count ||
+				                     ClientHardwareParts.Count == 0
+					? "<strong>полный комплект</strong>"
+					: "<strong style='color:red;'>неполный комплект</strong>, отсуствует:</br>" +
+					  string.Join(",</br>", ClientHardwareParts.Where(s => s.NotGiven).Select(o => "- " + o.Part.Name).ToList());
+				return clientHardware;
+			}else {
+				//возврат не полученных от клиента
+				var clientHardware = ClientHardwareParts.Count(s => !s.Absent) == Hardware.HardwareParts.Count ||
+				                     ClientHardwareParts.Count == 0
+					? "<strong>полный комплект</strong>"
+					: "<strong style='color:red;'>неполный комплект</strong>, отсутствует:</br>" +
+					  string.Join(",</br>", ClientHardwareParts.Where(s => s.Absent).Select(o => "- " + o.Part.Name).ToList());
+				return clientHardware;
+			}
 		}
 	}
 }
