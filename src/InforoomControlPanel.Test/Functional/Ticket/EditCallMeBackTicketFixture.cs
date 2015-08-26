62f78d6b82c6c7dc9ad152af5f505039501b04bc
Екатерина Дудочкина <edudochkina@analit.net>
2015-08-26 12:00:08 +0300

    i33675 Тесты по разделу Запросы пользователей в новой админке Inforoom

---
+++ src/InforoomControlPanel.Test/Functional/Ticket/EditCallMeBackTicketFixture.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Inforoom2.Models;
+using NHibernate.Linq;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace InforoomControlPanel.Test.Functional.Ticket
+{
+    class EditCallMeBackTicketFixture : TicketFixture
+    {
+        [Test, Description("Изменение запроса на обратный звонок")]
+        public void EditCallMeBackTicket()
+        {
+            //создаем запрос на обратный звонок
+            var ticketNew = new CallMeBackTicket();
+            ticketNew.PhoneNumber = "9685473196";
+            ticketNew.Text = "Не работает интернет";
+            ticketNew.Name = "Владимир";
+            DbSession.Save(ticketNew);
+
+            Open("Ticket/CallMeBackTicketIndex");
+            var callMeBackTicket = DbSession.Query<CallMeBackTicket>().First(p => p.PhoneNumber == "9685473196");
+            var targetTicket = browser.FindElementByXPath("//td[contains(.,'" + callMeBackTicket.PhoneNumber + "')]");
+            var row = targetTicket.FindElement(By.XPath("../.."));
+            var button = row.FindElement(By.CssSelector("a.btn-success"));
+            button.Click();
+            browser.FindElementByCssSelector(".btn-green").Click();
+            AssertText("Запрос на обратный звонок успешно изменен");
+            DbSession.Refresh(callMeBackTicket);
+            var employee = DbSession.Query<Employee>().First(p => p.Login == Environment.UserName);
+            Assert.That(callMeBackTicket.AnswerDate, Is.Not.Null, "Дата ответа на обратный звонок должна заполниться в базе данных");
+            Assert.That(callMeBackTicket.Employee, Is.EqualTo(employee), "Пользователь обработавший заявку на обратный звонок должен заполниться в базе данных");
+        }
+    }
+}
---
+++ src/InforoomControlPanel.Test/Functional/Ticket/EditTicketFixture.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Inforoom2.Models;
+using NHibernate.Linq;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace InforoomControlPanel.Test.Functional.Ticket
+{
+    class EditTicketFixture : TicketFixture
+    {
+        [Test, Description("Изменение запроса в техподдержку")]
+        public void EditTickets()
+        {
+            //создаем запрос в техподдержку
+            var employee = DbSession.Query<Employee>().First(p => p.Name == "test");
+            var ticketNew = new Inforoom2.Models.Ticket();
+            ticketNew.Employee = employee;
+            ticketNew.Email = "apopov@mail.ru";
+            ticketNew.Text = "Не работает интернет";
+            DbSession.Save(ticketNew);
+
+            Open("Ticket/TicketIndex");
+            var ticket = DbSession.Query<Inforoom2.Models.Ticket>().First(p => p.Email == "apopov@mail.ru");
+            var targetTicket = browser.FindElementByXPath("//td[contains(.,'" + ticket.Email + "')]");
+            var row = targetTicket.FindElement(By.XPath("../.."));
+            var button = row.FindElement(By.CssSelector("a.btn-success"));
+            button.Click();
+            browser.FindElementByCssSelector("textarea[id=ticket_Answer]").SendKeys("Проводятся технические работы");
+            browser.FindElementByCssSelector(".btn-green").Click();
+            AssertText("Ответ отправлен пользователю");
+            DbSession.Refresh(ticket);
+            Assert.That(ticket.IsNotified, Is.True, "Поле оповещение пользователя у запроса в техподдержку должно измениться");
+            Assert.That(ticket.AnswerDate, Is.Not.Null, "Дата ответа на запрос должна заполниться в базе данных");
+        }
+    }
+}
