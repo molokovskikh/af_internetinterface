3260fdaca30ff3d6c7c80cc550eb87cb434fb0d4
Екатерина Дудочкина <edudochkina@analit.net>
2015-09-11 12:08:28 +0300

    planChange Тесты по разделу Смена по истечению срока  в новой админке Inforoom

---
+++ src/InforoomControlPanel.Test/Functional/Plans/CreateInternetPlanChangerIndexFixture.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using Inforoom2.Models;
+using InforoomControlPanel.Test.Functional.infrastructure;
+using NHibernate.Linq;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace InforoomControlPanel.Test.Functional.Plans
+{
+	class CreateInternetPlanChangerFixture : PlanFixture
+	{
+		[Test, Description("Успешное добавление правила смены тарифов для услуги Internet")]
+		public void successfulCreateInternetPlanChanger()
+		{
+			Open("Plans/InternetPlanChangerIndex");
+			browser.FindElementByCssSelector("i.entypo-plus").Click();
+            Css(".targetPlan").SelectByText("50 на 50 (245 руб.)");
+            Css(".cheapPlan").SelectByText("Старт (245 руб.)");
+            Css(".fastPlan").SelectByText("Народный (300 руб.)");
+            browser.FindElementByCssSelector("input[id=planChanger_Timeout]").SendKeys("30");
+            browser.FindElementByCssSelector("textarea[id=planChanger_Text]").SendKeys("<p>Услуга Internet</p>");
+            browser.FindElementByCssSelector(".btn-green").Click();
+            AssertText("Объект успешно добавлен!");
+            var createPlanChanger =  DbSession.Query<PlanChangerData>().FirstOrDefault(p => p.Text == "<p>Услуга Internet</p>");
+            Assert.That(createPlanChanger, Is.Not.Null, "Правило смены тарифов должно сохраниться в базе данных");
+        }
+
+        [Test, Description("Не успешное добавление правила смены тарифов для услуги Internet")]
+        public void unsuccessfulCreateInternetPlanChanger()
+        {
+            var planChangerCount = DbSession.Query<PlanChangerData>().Count();
+            Open("Plans/InternetPlanChangerIndex");
+            browser.FindElementByCssSelector("i.entypo-plus").Click();
+            browser.FindElementByCssSelector(".btn-green").Click();
+            AssertText("выберите тариф");
+            DbSession.Flush();
+            var planChangerCountAfterTest = DbSession.Query<PlanChangerData>().Count();
+            Assert.That(planChangerCountAfterTest, Is.EqualTo(planChangerCount), "Правило смены тарифов не должно сохраниться в базе данных");
+        }
+    }
+}
---
+++ src/InforoomControlPanel.Test/Functional/Plans/EditInternetPlanChangerFixture.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Inforoom2.Models;
+using NHibernate.Linq;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace InforoomControlPanel.Test.Functional.Plans
+{
+    class EditInternetPlanChangerFixture : PlanFixture
+    {
+        [Test, Description("Изменение правила смены тарифов для услуги Internet")]
+        public void DeleteInternetPlanChanger()
+        {
+            var cheapPlan = DbSession.Query<Plan>().FirstOrDefault(p => p.Name == "50 на 50");
+            var fastPlan = DbSession.Query<Plan>().FirstOrDefault(p => p.Name == "Старт");
+            var targetPlan = DbSession.Query<Plan>().FirstOrDefault(p => p.Name == "Народный");
+            var planChange = new PlanChangerData();
+            planChange.CheapPlan = cheapPlan;
+            planChange.FastPlan = fastPlan;
+            planChange.TargetPlan = targetPlan;
+            planChange.Timeout = 30;
+            planChange.Text = "Второе правило услуги Internet";
+            DbSession.Save(planChange);
+
+            Open("Plans/InternetPlanChangerIndex");
+            var planChangeEdit = DbSession.Query<PlanChangerData>().FirstOrDefault(p => p.Text == "Второе правило услуги Internet");
+            var targetplanChange = browser.FindElementByXPath("//td[contains(.,'" + planChangeEdit.Text + "')]");
+            var row = targetplanChange.FindElement(By.XPath(".."));
+            var button = row.FindElement(By.CssSelector(".entypo-cancel-circled"));
+            button.Click();
+            Css(".targetPlan").SelectByText("50 на 50 (245 руб.)");
+            Css(".cheapPlan").SelectByText("Старт (245 руб.)");
+            Css(".fastPlan").SelectByText("Народный (300 руб.)");
+            browser.FindElementByCssSelector("input[id=planChanger_Timeout]").Clear();
+            browser.FindElementByCssSelector("input[id=planChanger_Timeout]").SendKeys("20");
+            browser.FindElementByCssSelector("textarea[id=planChanger_Text]").Clear();
+            browser.FindElementByCssSelector("textarea[id=planChanger_Text]").SendKeys("<p>Первое правило услуги Internet");
+            browser.FindElementByCssSelector(".btn-green").Click();
+            AssertText("Объект успешно изменен!");
+            DbSession.Refresh(planChangeEdit);
+            Assert.That(planChangeEdit.CheapPlan, Is.EqualTo(fastPlan), "Изменения в правиле смены тарифов для услуги Internet должны сохраниться корректно");
+            Assert.That(planChangeEdit.FastPlan, Is.EqualTo(targetPlan), "Изменения в правиле смены тарифов для услуги Internet должны сохраниться корректно");
+            Assert.That(planChangeEdit.TargetPlan, Is.EqualTo(cheapPlan), "Изменения в правиле смены тарифов для услуги Internet должны сохраниться корректно");
+            Assert.That(planChangeEdit.Timeout, Is.EqualTo(20), "Изменения в правиле смены тарифов для услуги Internet должны сохраниться корректно");
+            Assert.That(planChangeEdit.Text, Is.EqualTo("<p>Первое правило услуги Internet"), "Изменения в правиле смены тарифов для услуги Internet должны сохраниться корректно");
+        }
+    }
+}
